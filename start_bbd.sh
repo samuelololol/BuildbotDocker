bc70ae222300a9db23bda85898ef905423315c04
Samuel <samuel_chen@trend.com.tw>
2014-11-25 15:38:59 +0800

    edit start scripts

--- start_bbd.sh
+++ start_bbd.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-SOURCE_DIR="${BASH_SOURCE[0]}"
-SOURCE_DIR=$(dirname $SOURCE_DIR)
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 ID_FILE=$SOURCE_DIR"/bbd.id"
-CONTAINER_ID=`docker run -d samuelololol/gentoo-buildbot`
+BBD_DIR=$SOURCE_DIR"/buildbot"
+
+CONTAINER_NAME="bbd"
+HOST_BDD_PORT=8011
+CLIENT_MOUNT_POINT="/buildbot"
+
+
+CONTAINER_ID=`docker run -d --name bbd -p $HOST_BDD_PORT:8010 -v $BBD_DIR:$CLIENT_MOUNT_POINT samuelololol/gentoo-buildbot`
 echo $CONTAINER_ID > $ID_FILE
+

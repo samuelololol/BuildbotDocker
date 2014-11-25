ab13fc89c9646c2b65fc60430506a77a20fbadc0
Samuel <samuel_chen@trend.com.tw>
2014-11-25 14:42:02 +0800

    use buildbot image

--- start_bbd.sh
+++ start_bbd.sh
@@ -2,5 +2,5 @@
 SOURCE_DIR="${BASH_SOURCE[0]}"
 SOURCE_DIR=$(dirname $SOURCE_DIR)
 ID_FILE=$SOURCE_DIR"/bbd.id"
-CONTAINER_ID=`docker run -d samuelololol/gentoo-nginx`
+CONTAINER_ID=`docker run -d samuelololol/gentoo-buildbot`
 echo $CONTAINER_ID > $ID_FILE

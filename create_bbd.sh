19e2ab85272af6fc9a72432bbc9394cbe5f830a3
Samuel <samuel_chen@trend.com.tw>
2014-11-25 18:06:46 +0800

    change scripts names

--- create_bbd.sh
+++ create_bbd.sh
@@ -4,10 +4,15 @@ ID_FILE=$SOURCE_DIR"/bbd.id"
 BBD_DIR=$SOURCE_DIR"/buildbot"
 
 CONTAINER_NAME="bbd"
-HOST_BDD_PORT=8011
+HOST_BDD_PORT=8010
 CLIENT_MOUNT_POINT="/buildbot"
 
 
 CONTAINER_ID=`docker run -d --name bbd -p $HOST_BDD_PORT:8010 -v $BBD_DIR:$CLIENT_MOUNT_POINT samuelololol/gentoo-buildbot`
 echo $CONTAINER_ID > $ID_FILE
+echo "bbd sha1: "$CONTAINER_ID
+echo ""
+echo "To enter the container:"
+echo "docker exec -it $CONTAINER_ID" bash
+echo ""
 

32deeaf94d8adb84a2cf434763f45a7f7b1145a2
samuel <samuelololol@gmail.com>
2014-11-28 03:54:13 +0800

    refine prepare_image.sh script

--- prepare_image.sh
+++ prepare_image.sh
@@ -3,8 +3,30 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 ID_FILE=$SOURCE_DIR"/bbd.id"
 BBD_DIR=$SOURCE_DIR"/buildbot"
 
-IMAGE_NAME="bbd_img"
+if [ "$#" -ne 1 ]; then
+    echo "./prepare_image.sh <buildbot folder path>"
+    echo ""
+    echo "use default buildbot setting:"
+    echo "  $SOURCE_DIR/buildbot"
+else
+    echo ""
+    echo "use $1 as buildbot folder"
 
+    rm -rf $SOURCE_DIR/buildbot
+    BUILDBOT_FDR=$1
+    local_bbd_fdr_name=${1%/}
+    local_bbd_fdr_name=${local_bbd_fdr_name##*/}
+    if [[ -d $1 ]]; then
+        cp -r $1 .
+        if [ "$local_bbd_fdr_name" != "buildbot" ]; then
+            mv $local_bbd_fdr_name buildbot
+        fi
+    else
+        exit 1
+    fi
+fi
+
+IMAGE_NAME="bbd_img"
 CONTAINER_NAME="bbd"
 HOST_BDD_PORT=8010
 CLIENT_MOUNT_POINT="/buildbot"

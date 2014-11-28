d590f3828a68cb12abbce42361a83ede838d91ef
Samuel <samuel_chen@trend.com.tw>
2014-11-28 14:13:15 +0800

    edit remove_image.sh

--- remove_image.sh
+++ remove_image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 IMAGE_NAME="bbd_img"
-IMAGE_ID=`docker images | grep $IMAGE_NAME | awk '{print $3}'`
-docker rmi $IMAGE_ID
+docker images | grep $IMAGE_NAME | awk '{print $3}' | xargs docker rmi
 

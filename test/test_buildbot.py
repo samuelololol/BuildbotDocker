c36c39c14273fe4219ac540ac6737139637edf51
samuelololol <samuelololol@gmail.com>
2015-08-17 00:11:31 +0800

    add test to containers

---
+++ test/test_buildbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__date__= 'Aug 16, 2015 '
+__author__= 'samuel'
+
+import pytest
+import requests
+from BeautifulSoup import BeautifulSoup as bs
+import os
+
+
+def test_buildbot_availible():
+    response = requests.get('http://buildbotmaster:8010/json/builders/?as_text=1')
+    assert response.status_code == 200
+

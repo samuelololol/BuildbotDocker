cb670aaa79acbaae7b65398bf18aa5e40117a683
samuelololol <samuelololol@gmail.com>
2015-08-18 00:57:18 +0800

    change branches to branch, enable container removing, use docker-compose run to test

--- docker-test/tests/test_buildbot.py
+++ docker-test/tests/test_buildbot.py
@@ -8,14 +8,10 @@ import requests
 from requests.adapters import HTTPAdapter
 from BeautifulSoup import BeautifulSoup as bs
 import os
-import time
 
 
 def test_buildbot_availible():
-    #time.sleep(10)
     url = 'http://buildbotmaster:8010/json/builders/?as_text=1'
-    s = requests.Session()
-    s.mount(url, HTTPAdapter(max_retries=100))
     response = s.get(url)
     print 'ready to test http://buildbotmaster:8010/json/builders/?as_text=1'
     assert response.status_code == 200
--- docker-test/tests/test_runner.sh
+++ docker-test/tests/test_runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 5
-py.test $@
+py.test -s $@

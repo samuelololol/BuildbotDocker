aa398c4068d501d4c2b3b54de47603055ceee97e
samuelololol <samuelololol@gmail.com>
2015-08-17 17:59:40 +0800

    mount HTTPAdapter to requests

--- docker-test/tests/test_buildbot.py
+++ docker-test/tests/test_buildbot.py
@@ -5,6 +5,7 @@ __author__= 'samuel'
 
 import pytest
 import requests
+from requests.adapters import HTTPAdapter
 from BeautifulSoup import BeautifulSoup as bs
 import os
 import time
@@ -12,7 +13,10 @@ import time
 
 def test_buildbot_availible():
     #time.sleep(10)
-    response = requests.get('http://buildbotmaster:8010/json/builders/?as_text=1')
+    url = 'http://buildbotmaster:8010/json/builders/?as_text=1'
+    s = requests.Session()
+    s.mount(url, HTTPAdapter(max_retries=100))
+    response = s.get(url)
     print 'ready to test http://buildbotmaster:8010/json/builders/?as_text=1'
     assert response.status_code == 200
 
